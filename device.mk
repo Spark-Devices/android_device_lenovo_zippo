6cb53fcef748da3b2ff43d8298b748368d5d3e1e
Marc Bourgoin <themard85@gmail.com>
2021-08-30 23:03:23 -0600

    zippo: decommonize fm
    heart doesn't have a 3.5mm headphone jack. Only leave our bluetooth's fm blob
    dependency in sm8150-common and the rest can be moved to zippo.
    
    Change-Id: Id81e6df3fb5e03d1a19b9df3b0d5522bf766a577

--- device.mk
+++ device.mk
@@ -4,15 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Audio
-PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/zippo/zippo-vendor.mk)
 
@@ -23,6 +14,21 @@ $(call inherit-product, device/lenovo/sm8150-common/common.mk)
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := msmnile
 TARGET_BOARD_PLATFORM := msmnile
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
