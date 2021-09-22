31f768f64ac098bafaf133c765b937e9f499b81f
chematelegram <chfondia@gmail.com>
2021-09-23 00:47:45 +0200

    zippo: Don't build FP nor override PRIVATE_BUILD_DESC

--- spark_zippo.mk
+++ spark_zippo.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/lenovo/zippo/device.mk)
 # Inherit some common Spark OS stuff.
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS=true
+WITH_GAPPS=false
 WITH_FOD_ANIMATIONS=true
 TARGET_USES_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -23,10 +23,3 @@ PRODUCT_MODEL := Lenovo L78051
 PRODUCT_NAME := spark_zippo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
-    PRODUCT_NAME="zippo" \
-    TARGET_DEVICE="zippo"
-
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
