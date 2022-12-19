43cbc068607958be3fb944ad1897215bbd42a691
spkal01 <kalligeross@gmail.com>
2022-12-19 13:58:23 +0000

    zippo: Don't build Graphene camera

--- spark_zippo.mk
+++ spark_zippo.mk
@@ -13,6 +13,7 @@ EXTRA_UDFPS_ANIMATIONS := true
 TARGET_USES_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_NO_PREBUILT_CAMERA := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
